9152bca33c47262748764f8aa30a0c49867213e7
陈传真 <542320753@qq.com>
2020-11-07 21:38:07 +0800

    更新

---
+++ CZLibTest/CZLibTest/CZLibTest/BaseObject.h
@@ -0,0 +1,19 @@
+//
+//  BaseObject.h
+//  CZLibTest
+//
+//  Created by 陈传真 on 2020/11/7.
+//  Copyright © 2020 陈传真. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseObject : NSObject
+
++ (void)print;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CZLibTest/CZLibTest/CZLibTest/BaseObject.m
@@ -0,0 +1,15 @@
+//
+//  BaseObject.m
+//  CZLibTest
+//
+//  Created by 陈传真 on 2020/11/7.
+//  Copyright © 2020 陈传真. All rights reserved.
+//
+
+#import "BaseObject.h"
+
+@implementation BaseObject
++ (void)print {
+    NSLog(@"111");
+}
+@end
